c38d3087a7da89cd5341b89e4261347fe7f233fc
Amit Panda <amitpanda007@gmail.com>
2020-03-11 23:34:27 +0530

    matrix factorization initial commit

--- rec_app/api/recommend/matrix_factorization.py
+++ rec_app/api/recommend/matrix_factorization.py
@@ -4,40 +4,28 @@ import numpy as np
 from rec_app.api.recommend.utilities import low_rank_matrix_factorization
 
 
-# Load user ratings
-raw_dataset_df = pd.read_csv("")
+def low_rank_matrix(user_id):
+    raw_dataset_df = pd.read_csv("movie_ratings_data_set.csv")  # Load user ratings
+    movies_df = pd.read_csv("movies.csv")  # Load movie titles
+    ratings_df = pd.pivot_table(raw_dataset_df, index='user_id', columns='movie_id', aggfunc=np.max)  # Convert the running list of user ratings into a matrix
+    U, M = low_rank_matrix_factorization(ratings_df.values, num_features=15, regularization_amount=0.1)  # Apply matrix factorization to find the latent features
+    predicted_ratings = np.matmul(U, M)  # Find all predicted ratings by multiplying U and M matrices
 
-# Load movie titles
-movies_df = pd.read_csv("")
+    reviewed_movies_df = raw_dataset_df[raw_dataset_df['user_id'] == user_id]
+    reviewed_movies_df = reviewed_movies_df.merge(movies_df, on='movie_id')
 
-# Convert the running list of user ratings into a matrix
-ratings_df = pd.pivot_table(raw_dataset_df, index='user_id', columns='movie_id', aggfunc=np.max)
+    user_ratings = predicted_ratings[user_id - 1]
+    movies_df['rating'] = user_ratings
 
-# Apply matrix factorization to find the latent features
-U, M = low_rank_matrix_factorization(ratings_df.as_matrix(), num_features=15, regularization_amount=0.1)
+    already_reviewed = reviewed_movies_df['movie_id']
+    recommended_df = movies_df[movies_df.index.isin(already_reviewed) == False]
+    recommended_df = recommended_df.sort_values(by=['rating'], ascending=False)
+    return recommended_df[["title"]].head(6)
 
-# Find all predicted ratings by multiplying U and M matrices
-predicted_ratings = np.matmul(U, M)
 
-print("Enter a user_id to get recommendations (Between 1 and 100):")
-user_id_to_search = int(input())
+def recommend_new_user(user_id):
+    movies_to_recommend = low_rank_matrix(user_id)
+    return movies_to_recommend[1:]
 
-print("Movies previously reviewed by user_id {}:".format(user_id_to_search))
 
-reviewed_movies_df = raw_dataset_df[raw_dataset_df['user_id'] == user_id_to_search]
-reviewed_movies_df = reviewed_movies_df.join(movies_df, on='movie_id')
-
-print(reviewed_movies_df[['title', 'genre', 'value']])
-
-input("Press enter to continue.")
-
-print("Movies we will recommend:")
-
-user_ratings = predicted_ratings[user_id_to_search - 1]
-movies_df['rating'] = user_ratings
-
-already_reviewed = reviewed_movies_df['movie_id']
-recommended_df = movies_df[movies_df.index.isin(already_reviewed) == False]
-recommended_df = recommended_df.sort_values(by=['rating'], ascending=False)
-
-print(recommended_df[['title', 'genre', 'rating']].head(5))
+print(recommend_new_user(8))
--- rec_app/api/recommend/recommender.py
+++ rec_app/api/recommend/recommender.py
@@ -1,10 +1,12 @@
 import logging
 
+from flask import request
 from flask_restplus import Resource
 
 from rec_app.api.restplus import api
 from rec_app.database.models.user_ratings_model import UserRatings
-from .svd_recommend import svd_rec
+from .svd import svd_rec
+from .matrix_factorization import recommend_new_user
 
 log = logging.getLogger(__name__)
 
@@ -15,8 +17,9 @@ ns = api.namespace('recommend', description='Operations related to SVD recommend
 @ns.route("/default")
 class DefaultRecommender(Resource):
 
-    def get(self):
-        movies = svd_rec("Star Wars (1977)")
+    def post(self):
+        user_info = request.get_json()
+        movies = recommend_new_user(user_info["user_id"])
         return str(movies)
 
 
